8ddc0e4e722e1ff3601cb1bc2517e108e53498ec
TyOverby <ty@pre-alpha.com>
2012-08-20 21:24:21 -0700

    proved theory

--- shim.py
+++ shim.py
@@ -1,7 +1,7 @@
 class Dim:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
 
     def __str__(self):
         return "["+str(self.x)+","+str(self.y)+"]"
@@ -44,12 +44,13 @@ class Pixel:
         return str(self.pos)+" "+str(self.color)
 
 class Window:
-    def __init__(self, fname, title="title", dims=Dim(500,500), color=Color.black):
+    def __init__(self, fname=None, title="title", dims=Dim(500,500), color=Color.black):
         self.fname = fname
         self.id   = pmNewImage(0,title, dims.x, dims.y, color.r, color.g, color.b)
-        pmOpenImage(self.id, fname)
+        if fname is not None:
+            pmOpenImage(self.id, fname)
 
-    def getPixel(self, pos):
+    def getColor(self, pos):
         value = pmGetPixelQuickly(self.id, pos.x, pos.y)
         r = value / (256*256)
         g = (value - (r*256*256))/256
@@ -72,10 +73,10 @@ class Window:
                 yield Dim(x, y)
     def eachColor(self):
         for pos in self.eachPos():
-            yield getPixel(pos)
+            yield self.getColor(pos)
     def eachPixel(self):
         for pos in self.eachPos():
-            yield Pixel(pos, self.getPixel(pos), self)
+            yield Pixel(pos, self.getColor(pos), self)
 
     def pixelsMatching(self, func):
         for pixel in self.eachPixel():
@@ -91,18 +92,43 @@ class Window:
                 bestValue = current
                 best = pixel
         return best;
+    def inBounds(self, position):
+        size = self.getSize()
+        x = size.x>position.x and 0 < position.x
+        y = size.y>position.y and 0 < position.y
+        return x and y
 
-window = Window(fname = "C:\\Users\\Ty\Pictures\\blur\\fb2.jpg", dims=Dim(500,400))
+picture = Window(fname = "C:\\Users\\Ty\Pictures\\blur\\fb2.jpg", dims=Dim(500,400))
+blank   = Window(dims=Dim(500,400))
+
+scaleSize = 5
+
+oldSize  = picture.getSize()
+halfSize = Dim(oldSize.x/2,oldSize.y/2)
+newSize  = Dim(oldSize.x/scaleSize,oldSize.y/scaleSize)
+for blankPix in blank.eachPixel():
+    oldPos = blankPix.pos
+    xmod = halfSize.x - newSize.x/2
+    ymod = halfSize.y - newSize.y/2
+    #print xmod, ymod
+    modPos = Dim((oldPos.x-xmod)*scaleSize ,(oldPos.y-ymod)*scaleSize)
+
+    if picture.inBounds(modPos):
+        blankPix.setColor(picture.getColor(modPos))
+
+blank.refresh()
+
+
 
 # Example of lambda select
-white = Color(255,255,255)
-for pixel in window.eachPixel(): #window.pixelsMatching(lambda p: p.color.r<50 and p.color.g<50 and p.color.b<50):
-    pixel.color.r = 255-pixel.color.r
-    pixel.color.g = 255-pixel.color.g
-    pixel.color.b = 255-pixel.color.b
-    pixel.setColor()
+#white = Color(255,255,255)
+#for pixel in window.eachPixel(): #window.pixelsMatching(lambda p: p.color.r<50 and p.color.g<50 and p.color.b<50):
+#    pixel.color.r = 255-pixel.color.r
+#    pixel.color.g = 255-pixel.color.g
+#    pixel.color.b = 255-pixel.color.b
+#    pixel.setColor()
 
-window.refresh()
+#window.refresh()
 
 #Set the red component of each Pixel to full
 #for pixel in window.eachPixel():
