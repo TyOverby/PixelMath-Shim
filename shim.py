f2739c3cf0ab334cb14614f01f34306e08a0a4df
TyOverby <ty@pre-alpha.com>
2012-08-20 17:25:51 -0700

    extended functionality and started making some tests

--- shim.py
+++ shim.py
@@ -3,8 +3,11 @@ class Dim:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return "["+str(self.x)+","+str(self.y)+"]"
+
 class Color:
-    def __init__(self, red, green, blue, tup=None):
+    def __init__(self, red=0, green=0, blue=0, tup=None):
         if tup is not None:
             self.r = tup[0]
             self.g = tup[1]
@@ -13,14 +16,32 @@ class Color:
             self.r = red;
             self.g = green;
             self.b = blue;
+    def getTrue(self):
+        if -1 not in (self.r, self.g, self.b):
+            return int((256*256*float(self.r))+(256*float(self.g))+(self.b))
+        else:
+            return 1
+
+    def __str__(self):
+        return "{r:"+str(self.r)+", g:"+str(self.g)+", b:"+str(self.b)+"}"
 
 Color.black = Color(0,0,0)
 Color.white = Color(255,255,255)
 
 class Pixel:
-    def __init__(self, pos, color):
-        self.pos   = pos
-        self.color = color
+    def __init__(self, pos, color, window):
+        self.pos    = pos
+        self.color  = color
+        self.window = window
+
+    def setColor(self, color=None):
+        if color is not None:
+            self.window.setPixel(self.pos, color)
+        else:
+            self.window.setPixel(self.pos, self.color)
+
+    def __str__(self):
+        return str(self.pos)+" "+str(self.color)
 
 class Window:
     def __init__(self, fname, title="title", dims=Dim(500,500), color=Color.black):
@@ -28,30 +49,63 @@ class Window:
         self.id   = pmNewImage(0,title, dims.x, dims.y, color.r, color.g, color.b)
         pmOpenImage(self.id, fname)
 
-    def getPixel(pos):
-        return pmGetPixel(self.id, pos.x, pos.y)
+    def getPixel(self, pos):
+        value = pmGetPixelQuickly(self.id, pos.x, pos.y)
+        r = value / (256*256)
+        g = (value - (r*256*256))/256
+        b = (value - (r*256*256) - (g*256))
+        return Color(r,g,b)
 
-    def setPixel(pos, color):
-        pmSetPixel(self.id, pos.x, pos.y, color.r, color.g, color.b)
+    def setPixel(self, pos, color):
+        # pmSetPixel(self.id, pos.x, pos.y, color.r, color.g, color.b)
+        pmSetPixelQuickly(self.id, pos.x, pos.y, color.getTrue())
+    def getSize(self):
+        return Dim(pmGetImageWidth(self.id), pmGetImageHeight(self.id))
 
-    def getSize():
-        size = pmGetScreenSize()
-        return Dim(size[0],size[1])
+    def refresh(self):
+        pmUpdateDisplay(self.id)
 
-    def refresh():
-        pmRefresh(self.id)
-
-    def eachPos():
+    def eachPos(self):
         size = self.getSize()
-        for x in range(0, size.x):
-            for y in range(0, size.y):
+        for x in range(0, size.x-1):
+            for y in range(0, size.y-1):
                 yield Dim(x, y)
-    def eachColor():
+    def eachColor(self):
         for pos in self.eachPos():
             yield getPixel(pos)
-    def eachPixel():
+    def eachPixel(self):
         for pos in self.eachPos():
-            yield Pixel(pos, getPixel(pos))
+            yield Pixel(pos, self.getPixel(pos), self)
 
+    def pixelsMatching(self, func):
+        for pixel in self.eachPixel():
+            if func(pixel):
+                yield pixel
 
+    def pixelsCompared(self, func):
+        bestValue = -2147483646
+        best = None
+        for pixel in self.eachPixel():
+            current = func(pixel)
+            if current>best:
+                bestValue = current
+                best = pixel
+        return best;
 
+window = Window(fname = "C:\\Users\\Ty\Pictures\\blur\\fb2.jpg", dims=Dim(500,400))
+
+# Example of lambda select
+white = Color(255,255,255)
+for pixel in window.eachPixel(): #window.pixelsMatching(lambda p: p.color.r<50 and p.color.g<50 and p.color.b<50):
+    pixel.color.r = 255-pixel.color.r
+    pixel.color.g = 255-pixel.color.g
+    pixel.color.b = 255-pixel.color.b
+    pixel.setColor()
+
+window.refresh()
+
+#Set the red component of each Pixel to full
+#for pixel in window.eachPixel():
+#    pixel.color.r = 255;
+#    pixel.set();
+#window.refresh();
